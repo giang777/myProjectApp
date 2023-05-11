3329f98632970f25211f276e2a32bd085f438511
giang777 <cucosieu@gmail.com>
2023-05-11 15:55:09 +0700

    up

---
+++ server/controller/main.js
@@ -0,0 +1,16 @@
+const firebase = require("../model/data.js");
+
+exports.GetList = async (req,res,next)=>{
+    const data = firebase.ref("User");
+    const list =  await data.once('value');
+    const listUser = [];
+    list.forEach((item)=>{
+        listUser.push({
+            id:item.key,
+            ...item.val(),
+        })
+    });
+    console.log(listUser);
+
+    res.render("home/index");
+}
